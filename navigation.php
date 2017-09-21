b787241a43327e970a1ce52b07ef075d8397ef8a
Ville Aho <Ville@Villes-MBP.home>
2017-09-21 16:12:33 +0300

    Yläpalkin ylimääräisten li tagien poisto

--- navigation.php
+++ navigation.php
@@ -1,15 +1,10 @@
 <nav class="nav">
-<<<<<<< HEAD
-  <a class="nav-link" href="index.php">Logo</a></li>
-  <a class="nav-link" href="#">Playlist</a></li>
-      <form action="search.php" class="nav-item form-inline my-2 my-lg-0">
-        <input class="form-control mr-sm-2" type="text" name="q" placeholder="Search" aria-label="Search">
-=======
-  <a class="nav-link" href="index.php">Logo</a>
-  <a class="nav-link" href="#">Playlist</a>
-      <form class="nav-item form-inline my-2 my-lg-0">
-        <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
->>>>>>> layout
-        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-      </form>
-</nav>
+    <a class="nav-link" href="index.php">Logo</a>
+    <a class="nav-link" href="#">Playlist</a>
+ -      <form class="nav-item form-inline my-2 my-lg-0">
+ -        <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+ +      <form action="search.php" class="nav-item form-inline my-2 my-lg-0">
+ +        <input class="form-control mr-sm-2" type="text" name="q" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+  </nav>
