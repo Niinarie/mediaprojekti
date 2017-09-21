9fe10fc90352dfe3df58873d2f1d81d6b2ddf96b
Ville Aho <Ville@Villes-MBP.home>
2017-09-21 16:01:24 +0300

    Yläpalkin turhat </li> tagit pois

--- navigation.php
+++ navigation.php
@@ -1,6 +1,6 @@
 <nav class="nav">
-  <a class="nav-link" href="index.php">Logo</a></li>
-  <a class="nav-link" href="#">Playlist</a></li>
+  <a class="nav-link" href="index.php">Logo</a>
+  <a class="nav-link" href="#">Playlist</a>
       <form class="nav-item form-inline my-2 my-lg-0">
         <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
         <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
