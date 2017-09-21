a365a11b60b68ede2bc7e2069cccea4df5064c7f
Ville Aho <Ville@Villes-MBP.home>
2017-09-21 16:31:24 +0300

    Yläpalkin haku oikealle

--- navigation.php
+++ navigation.php
@@ -2,7 +2,6 @@
     <a class="nav-link" href="index.php">Logo</a>
     <a class="nav-link" href="#">Playlist</a>
     <form class="nav-item form-inline my-2 my-lg-0">
-    <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
     <form action="search.php" class="nav-item form-inline my-2 my-lg-0">
         <input class="form-control mr-sm-2" type="text" name="q" placeholder="Search" aria-label="Search">
         <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
