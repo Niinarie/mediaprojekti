9653e973baee05e1c043083c94ceda266da9ed52
Niinarie <pocky.hero@gmail.com>
2017-10-04 19:17:42 +0300

    single page

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -74,6 +74,10 @@ const apiURL = "http://localhost:3000/";
   }
 
   var getAllVideos = function() {
+    if($('#videoPlayer').length) {
+      videojs('videoPlayer').dispose();
+    }
+    $('#video').css({display: 'none'});
     $.ajax({
       url: apiURL+ "videos",
       type: "GET",
@@ -81,61 +85,65 @@ const apiURL = "http://localhost:3000/";
     }).done(function( data ) {
       data = checkAllFavs(data);
       console.log(data);
-      $('#gridFresh').html('');
+      $('#main').html('');
       var counter = 1;
+      $('#main').append('<h2>Fresh:</h2><div id="gridFresh" class="grid">')
       $.each(data, function(key, value){
           var html = '';
           html += '<div class="grid-item';
           if (counter == 1 || counter == 2) {
           html += ' grid-item--width2'
           };
-          html += '"><img src="'+value.poster+'"/>';
-          html += '<a href="player.php?id='+value.id+'"><h3 class="h3--grid">'+value.name+'</h3></a>';
+          html += '" id="video'+value.id+'"><img src="'+value.poster+'"/>';
+          html += '<h3 class="h3--grid">'+value.name+'</h3>';
           html += '<button class="button--favourite';
           {value.isliked ? html +=' button--remove" ' : html+=' button--add" '};
           html += 'data-favId='+value.id+'><i class="fa fa-2x fa-star" aria-hidden="true"></i></button>';
-          html += '<a href="player.php?id='+value.id+'"><div class="description"><p>'+value.description+'</p>';
+          html += '<div class="description grid-link" data-id='+value.id+'><p>'+value.description+'</p>';
           html += '<div class="tags"><i class="fa fa-tags" aria-hidden="true"></i>';
           $.each(value.tags, function(key,value) {
             html += '<span class="tag">'+value+'</span>';
           });
-          html += '</div></div></a></div>';
+          html += '</div></div></div>';
           if (counter < 5) {
             counter = counter + 1;
           } else {
             counter = 1;
           }
           $('#gridFresh').append(html);
-          $('.button--favourite').off().on('click', function(e){
-            if ($(this).hasClass('button--add')) {
-              $(this).removeClass('button--add');
-              $(this).addClass('button--remove');
-              addToFav($(this).data('favid'));
-           } else {
-            $(this).removeClass('button--remove');
-            $(this).addClass('button--add');
-            removeFav($(this).data('favid')); 
-            }
-          })
+          $('#video'+value.id).data('value',value);
       });     
+      $('.button--favourite').off().on('click', function(e){
+        if ($(this).hasClass('button--add')) {
+          $(this).removeClass('button--add');
+          $(this).addClass('button--remove');
+          addToFav($(this).data('favid'));
+       } else {
+        $(this).removeClass('button--remove');
+        $(this).addClass('button--add');
+        removeFav($(this).data('favid')); 
+        }
+      })
+      $('.grid-link').click(function(e) {
+       playVideo($(this).parent().data('value'));
+       window.scrollTo(0, 0);
+      })
     });
   }
 
-  var getVideoInfo = function(id) {
-    $.ajax({
-      url: apiURL+ "videos/"+id,
-      type: "GET",
-      contentType: "application/json"
-    }).done(function( data ) {
-      console.log(data);
-      videojs('videoPlayer', {
-        controls: true,
-        autoplay: false,
-        preload: 'none',
-        poster: data.poster,      
-      });
-      if (data.subs) {
-        $.each(data.subs, function(key, value){
+  var playVideo = function(video) {
+    if($('#videoPlayer').length) {
+      videojs('videoPlayer').dispose();
+    }
+    $('#video__div').html('<video id="videoPlayer" class="video-js vjs-fluid"></video>');
+    videojs('videoPlayer', {
+      controls: true,
+      autoplay: true,
+      preload: 'none',
+      poster: video.poster
+    });
+      if (video.subs) {
+        $.each(video.subs, function(key, value){
          videojs('videoPlayer').addRemoteTextTrack({
            src: 'subs/'+value.src,
            kind: value.kind,
@@ -145,22 +153,26 @@ const apiURL = "http://localhost:3000/";
           })
         });
       }
-      videojs('videoPlayer').src(data.src);
-      $('#videoTitle').html('<h2>'+data.name+'</h2>');
-      $('#videoDescription').html('<p>'+data.description+'</p>');
-      $('#videoTags').append('<i class="fa fa-tags" aria-hidden="true"></i>');
-      $.each(data.tags, function(key, value) {
+      videojs('videoPlayer').src(video.src);
+      $('#videoTitle').html('<h2>'+video.name+'</h2>');
+      $('#videoDescription').html('<p>'+video.description+'</p>');
+      $('#videoTags').html('<i class="fa fa-tags" aria-hidden="true"></i>');
+      $.each(video.tags, function(key, value) {
         $('#videoTags').append('<span class="player__tags-tag" data-tag='+value+'>'+value+'</span>');
       });
 
       $('.player__tags-tag').click(function(e){
-        window.location.replace("search.php?q="+$(this).data('tag'));
+        videoSearch($(this).data('tag'));
       });
-
-    });
-  }
+      $('#video').css({ display: 'block'});
+ }
 
   var videoSearch = function(tags) {
+    if($('#videoPlayer').length) {
+      videojs('videoPlayer').dispose();
+    }
+    $('#video').css({display: 'none'});
+    $('#main').html('Loading...');
     console.log(tags);
     $.ajax({
       url: apiURL+ "videos/search/"+tags,
@@ -168,31 +180,51 @@ const apiURL = "http://localhost:3000/";
       contentType: "application/json"
     }).done(function( data ) {
       if (data.length> 0) {
+        $('#main').html('<h2>Search: '+tags+'</h2><div class="grid" id="searchGrid"></div>');
         data = checkAllFavs(data);
-        $('#searchTitle').html('<h2>Search: '+tags+'</h2');
-        $('#searchGrid').html('');
         $.each(data, function(key, value){
           var html = '';
-          html += '<div class="grid-item grid-item-small"><img src="'+value.poster+'"/>';
-          html += '<a href="player.php?id='+value.id+'"><h3 class="h3--grid">'+value.name+'</h3></a>';
+          html += '<div class="grid-item grid-item-small" id="video'+value.id+'"><img src="'+value.poster+'"/>';
+          html += '<h3 class="h3--grid">'+value.name+'</h3>';
           html += '<button class="button--favourite';
           {value.isliked ? html +=' button--remove" ' : html+=' button--add" '};
           html += 'data-favId='+value.id+'><i class="fa fa-2x fa-star" aria-hidden="true"></i></button>';
-          html += '<a href="player.php?id='+value.id+'"><div class="description"><p>'+value.description+'</p>';
+          html += '<div class="description grid-link"><p>'+value.description+'</p>';
           html += '<div class="tags"><i class="fa fa-tags" aria-hidden="true"></i>';
           $.each(value.tags, function(key,value) {
             html += '<span class="tag">'+value+'</span>';
           });
-          html += '</div></div></a></div>';
+          html += '</div></div></div>';
           $('#searchGrid').append(html);
+          $('#video'+value.id).data('value',value);
       });
+      $('.button--favourite').off().on('click', function(e){
+        if ($(this).hasClass('button--add')) {
+          $(this).removeClass('button--add');
+          $(this).addClass('button--remove');
+          addToFav($(this).data('favid'));
+       } else {
+        $(this).removeClass('button--remove');
+        $(this).addClass('button--add');
+        removeFav($(this).data('favid')); 
+        }
+      })
+      $('.grid-link').click(function(e) {
+        playVideo($(this).parent().data('value'));
+        window.scrollTo(0, 0);
+      })
       } else {
-        $('#searchGrid').html("No results");
+        $('#main').html('<h2>Search: '+tags+'</h2><div class="grid"">No results</div>');
       }
     });
   }
 
   var favouriteSearch = function() {
+    if($('#videoPlayer').length) {
+      videojs('videoPlayer').dispose();
+    }
+    $('#video').css({display: 'none'});
+    $('#main').html('Loading...');
     let favourites = getFavs();
     console.log(favourites.toString());
     $.ajax({
@@ -203,9 +235,25 @@ const apiURL = "http://localhost:3000/";
       }
     }).done(function( data ) {
       if (data.length> 0) {
+        $('#main').html('So fav!');
         console.log(data);
       } else {
-        $('#favouriteGrid').html("No favourites");
+        $('#main').html("No favourites");
       }
     });
   }
+
+  $('#searchBtn').click(function(e){
+    e.preventDefault();
+    videoSearch($('#searchFrm').val());
+  });
+
+  $('.link-home').click(function(e) {
+    e.preventDefault();
+    getAllVideos()
+  });
+
+  $('#favourites').click(function(e){
+    e.preventDefault();
+    favouriteSearch();
+  })
